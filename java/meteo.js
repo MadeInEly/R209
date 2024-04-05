8df44f685ce9b3ac9d41f53ff3826907edfca0bb
LaTransition <114809157+LaTransition@users.noreply.github.com>
2024-04-05 15:00:48 +0200

    ds

--- java/meteo.js
+++ java/meteo.js
@@ -3,3 +3,11 @@ let button = document.getElementById("recherche")
 button.addEventListener("click", function() {
     console.log(input.value)
 });
+
+let codePostal = document.getElementById('CP')
+codePostal.addEventListener('input', () => {
+    let CPsaisi = codePostal.value
+    if (CPsaisi.length === 5) {
+        console.log('valeur:' , CPSaisi))
+    }
+})
