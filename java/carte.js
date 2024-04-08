316a36df098e059aef93383645d2fa2a2e3f75aa
MadeInEly <166015665+MadeInEly@users.noreply.github.com>
2024-04-08 18:45:07 +0200

    Add files via upload

---
+++ java/carte.js
@@ -0,0 +1,18 @@
+const map = L.map('map').setView([48.8566, 2.3522], 5); // Coordonnées de Paris (centre de la France), zoom de départ : 5
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+map.on('click', async function(e) {
+    const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${e.latlng.lat}&lon=${e.latlng.lng}`);
+    const data = await response.json();
+    
+    if (data.address && data.address.postcode) {
+        const codePostal = data.address.postcode;
+        console.log('Numéro de la commune:', codePostal);
+    } else {
+        console.log('Aucune commune trouvée à cet emplacement.');
+    }
+});
+
